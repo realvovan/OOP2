eb3c63b98d9972897b5769f8abd6c542911ba6ec
Vovan <vovagor1234321@gmail.com>
2025-10-09 10:57:45 +0300

    Added lab 4

---
+++ labs/lab3.4/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace lab3_4;
+
+public delegate bool StringCheck(string str,char c);
+
+static class Program {
+	static void Main() {
+		Console.WriteLine("==============\nPart 1\n==============");
+		StringCheck anonymous = delegate (string str,char c) {
+			foreach (var i in str) {
+				if (c == i) return true;
+			}
+			return false;
+		};
+		bool anonResult = anonymous.Invoke("Hello world",'l');
+		Console.WriteLine($"Anonymous function result: {anonResult}");
+		StringCheck lambda = (str,c) => {
+			foreach (var i in str) {
+				if (c == i) return true;
+			}
+			return false;
+		};
+		bool lambdaResult = lambda.Invoke("Lorem ipsum",'z');
+		Console.WriteLine($"Lambda function result: {lambdaResult}");
+		Console.WriteLine("==============\nPart 1\n==============");
+		var stringQueue = new MyQueue<string>("hello world this is my program".Split(' '));
+		stringQueue.QueueCleared += onCleared;
+		stringQueue.ElementAdded += onElementAdded;
+		stringQueue.ElementRemoved += onElementRemoved;
+		stringQueue.Enqueue("C#");
+		Console.WriteLine($"Length after adding is {stringQueue.Length}");
+		Console.WriteLine($"Peeking: {stringQueue.Peek()}");
+		Console.WriteLine($"Popping: {stringQueue.Pop()}");
+		Console.WriteLine($"Length after popping is {stringQueue.Length}");
+		stringQueue.Pop();
+		stringQueue.Pop();
+		stringQueue.Pop();
+		Console.WriteLine($"Popping: {stringQueue.Pop()}");
+		stringQueue.Clear();
+		Console.WriteLine($"Length after clearing is {stringQueue.Length}");
+	}
+	static void onCleared(object? sender,EventArgs e) {
+		Console.WriteLine("Queue cleared");
+	}
+	static void onElementRemoved(object? sender, QueueChangedEventArgs<string> e) {
+		Console.WriteLine($"Element '{e.Element}' removed");
+	}
+	static void onElementAdded(object? sender,QueueChangedEventArgs<string> e) {
+		Console.WriteLine($"Element '{e.Element}' added");
+	}
+}
---
+++ labs/lab3.4/Queue.cs
@@ -0,0 +1,49 @@
+﻿namespace lab3_4;
+
+public class MyQueue<T> {
+	private readonly LinkedList<T> elements;
+	public int Length => this.elements.Count;
+	public event EventHandler? QueueCleared;
+	public event EventHandler<QueueChangedEventArgs<T>>? ElementAdded;
+	public event EventHandler<QueueChangedEventArgs<T>>? ElementRemoved;
+
+	public void Enqueue(T element) {
+		this.elements.AddLast(element);
+		this.ElementAdded?.Invoke(this, new QueueChangedEventArgs<T>(element));
+	}
+	public T Pop() {
+		if (this.elements.Count == 0) throw new InvalidOperationException("Queue is empty");
+		T element = this.elements.First!.Value;
+		this.elements.RemoveFirst();
+		this.ElementRemoved?.Invoke(this,new QueueChangedEventArgs<T>(element));
+		if (this.elements.Count == 0) this.onQueueCleared();
+		return element;
+	}
+	public T Peek() {
+		var first = this.elements.First;
+		if (first == null) throw new InvalidOperationException("Queue is empty");
+		return first.Value;
+	}
+	public void Clear() {
+		this.elements.Clear();
+		this.onQueueCleared();
+	}
+	public MyQueue() {
+		this.elements = new LinkedList<T>();
+	}
+	public MyQueue(T element) {
+		this.elements = new LinkedList<T>();
+		this.elements.AddLast(element);
+	}
+	public MyQueue(IEnumerable<T> elements) {
+		this.elements = new LinkedList<T>(elements);
+	}
+
+	private void onQueueCleared() {
+		this.QueueCleared?.Invoke(this, EventArgs.Empty);
+	}
+}
+
+public class QueueChangedEventArgs<T>(T element) : EventArgs {
+	public T Element { get; } = element;
+}
