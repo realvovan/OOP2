97b90c61838638d248fd25ab260c5c872f692a22
Vovan <vovagor1234321@gmail.com>
2026-03-02 16:39:10 +0200

    SoftwareArch LAB2: Added AnimalFactory

---
+++ Software architecture/lab1/AnimalFactory.cs
@@ -0,0 +1,13 @@
+﻿namespace SoftwareArch.lab1;
+
+public static class AnimalFactory {
+	private static readonly Dictionary<string,Func<string,Habitat,Animal>> registry = new();
+
+	public static void Register(string typeName,Func<string,Habitat,Animal> creator) {
+		registry[typeName] = creator;
+	}
+	public static Animal Create(string type,string name,Habitat habitat) {
+		if (!registry.TryGetValue(type,out var creator)) throw new ArgumentException("No creator found for provided type",nameof(type));
+		return creator(name, habitat);
+	}
+}
